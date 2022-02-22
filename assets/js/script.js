5eb27d8ba00cc765c5c421710f90680aa91c113e
shahery <shaheryar924@gmail.com>
2022-02-22 12:04:38 +0000

    Add comments in js and credits the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+let userName = "";
+
 /**
  * submitUsername function with the 
  * welcome Username message
- */
+ * @param {*} event a parameter that stores the click event value.
+*/
 
-let userName = "";
 function submitUsername (event) {
     if (usernameEmpty!= ""){
         let node = document.getElementsByClassName('info-area');
@@ -40,12 +42,14 @@ function emptyUsername() {
 let myButton = document.getElementById('my-btn');
 myButton.addEventListener('click', submitUsername);
 
-/**
- * The main quiz "loop", called when the script is first loaded
- * and after the user's answer has been processed
- **/
-
 let level;
+
+/**
+ * This function is used to select the level.
+ * It also gives the alert message if user does not select any level.
+ * @param {*} event a parameter that stores the click event value.
+ */
+
 function selectLevel (event) {
     if(document.getElementById('intermediate').checked || document.getElementById('beginner').checked || document.getElementById('advanced').checked){
      level = document.querySelector('input[name="level"]:checked').value;
@@ -65,6 +69,10 @@ function selectLevel (event) {
 let myButton2 = document.getElementById('my-level-btn');
 myButton2.addEventListener('click', selectLevel);
 
+/**
+ * showLevel function is used to display beginner, intermediate and advanced level questions.
+ * These displays are different in vertical height than others pages display.
+*/
 
 function showLevel(){
     
@@ -91,11 +99,15 @@ function showLevel(){
     }
 }
 
+const userAnswers=["","","","",""];
+
 /**
  * submitAnswers for beginner,intermediate and advanced questions.
- */
-
-const userAnswers=["","","","",""];
+ * User has to answer all the questions
+ * If user skip to answer any question and click submit button it will give the alert.
+ * Alert: Please answer all the questions.
+ * @param {*} event a parameter that stores the click event value.
+*/
 
 function submitAnswer (event) {
     let box = document.getElementsByClassName('quiz')[0];
@@ -123,62 +135,78 @@ function submitAnswer (event) {
     var gameSelected = check('game');
     var advancedSelected = false;
 
-        if(level=='beginner'){
-            if(capitalSelected && planetSelected && currencySelected && symbolSelected && presidentSelected){
-             let node = document.getElementById('beginner-question');
-             node.style.display='none';
-            
-             userAnswers[0]=(document.querySelector('input[name="capital"]:checked').value);
-             userAnswers[1]=(document.querySelector('input[name="planet"]:checked').value);
-             userAnswers[2]=(document.querySelector('input[name="currency"]:checked').value);
-             userAnswers[3]=(document.querySelector('input[name="symbol"]:checked').value);
-             userAnswers[4]=(document.querySelector('input[name="president"]:checked').value);
+    if(level=='beginner'){
+        if(capitalSelected && planetSelected && currencySelected && symbolSelected && presidentSelected){
+         let node = document.getElementById('beginner-question');
+         node.style.display='none';
 
-             for (let index = 0; index < userAnswers.length; index++) {
-                 console.log(userAnswers[index]);        
-                }
-             beginnerSelected=true;
+          // The code used below of queryselector was taken from stackoverflow //
+
+         userAnswers[0]=(document.querySelector('input[name="capital"]:checked').value);
+         userAnswers[1]=(document.querySelector('input[name="planet"]:checked').value);
+         userAnswers[2]=(document.querySelector('input[name="currency"]:checked').value);
+         userAnswers[3]=(document.querySelector('input[name="symbol"]:checked').value);
+         userAnswers[4]=(document.querySelector('input[name="president"]:checked').value);
+
+         for (let index = 0; index < userAnswers.length; index++) {
+             console.log(userAnswers[index]);        
             }
+            beginnerSelected=true;
+        }
         
-        }
-        if(level=='intermediate'){
-            if(drinkSelected && playersSelected && timesSelected && girlSelected && citySelected){
-             let node = document.getElementById('intermediate-questions');
-             node.style.display='none';
-             userAnswers[0]=(document.querySelector('input[name="drink"]:checked').value);
-             userAnswers[1]=(document.querySelector('input[name="players"]:checked').value);
-             userAnswers[2]=(document.querySelector('input[name="times"]:checked').value);
-             userAnswers[3]=(document.querySelector('input[name="girl"]:checked').value);
-             userAnswers[4]=(document.querySelector('input[name="city"]:checked').value);
+    }
+    if(level=='intermediate'){
+        if(drinkSelected && playersSelected && timesSelected && girlSelected && citySelected){
+         let node = document.getElementById('intermediate-questions');
+         node.style.display='none';
 
-             for (let index = 0; index < userAnswers.length; index++) {
-                 console.log(userAnswers[index]);        
-                }
-                intermediateSelected = true;
+         // The code used below of queryselector was taken from stackoverflow //
+
+         userAnswers[0]=(document.querySelector('input[name="drink"]:checked').value);
+         userAnswers[1]=(document.querySelector('input[name="players"]:checked').value);
+         userAnswers[2]=(document.querySelector('input[name="times"]:checked').value);
+         userAnswers[3]=(document.querySelector('input[name="girl"]:checked').value);
+         userAnswers[4]=(document.querySelector('input[name="city"]:checked').value);
+
+         for (let index = 0; index < userAnswers.length; index++) {
+                console.log(userAnswers[index]);        
             }
+            intermediateSelected = true;
+        }
         
-        }
-        if(level=='advanced'){
-            if(bodypartSelected && StateSelected && countrySelected && gameSelected && zoneSelected){
-             let node = document.getElementById('advanced-questions');
-             node.style.display='none';
-             userAnswers[0]=(document.querySelector('input[name="zone"]:checked').value);
-             userAnswers[1]=(document.querySelector('input[name="body-part"]:checked').value);
-             userAnswers[2]=(document.querySelector('input[name="State"]:checked').value);
-             userAnswers[3]=(document.querySelector('input[name="country"]:checked').value);
-             userAnswers[4]=(document.querySelector('input[name="game"]:checked').value);
+    }
+    if(level=='advanced'){
+        if(bodypartSelected && StateSelected && countrySelected && gameSelected && zoneSelected){
+         let node = document.getElementById('advanced-questions');
+         node.style.display='none';
 
-             for (let index = 0; index < userAnswers.length; index++) {
-                 console.log(userAnswers[index]);        
-                }
-                advancedSelected = true;
+         // The code used below of queryselector was taken from stackoverflow //
+
+         userAnswers[0]=(document.querySelector('input[name="zone"]:checked').value);
+         userAnswers[1]=(document.querySelector('input[name="body-part"]:checked').value);
+         userAnswers[2]=(document.querySelector('input[name="State"]:checked').value);
+         userAnswers[3]=(document.querySelector('input[name="country"]:checked').value);
+         userAnswers[4]=(document.querySelector('input[name="game"]:checked').value);
+
+          for (let index = 0; index < userAnswers.length; index++) {
+             console.log(userAnswers[index]);        
             }
+            advancedSelected = true;
         }
-        if(beginnerSelected || intermediateSelected || advancedSelected)
-        showResult();
-        else
-        alert("Please answer all the questions"); 
+    }
+    if(beginnerSelected || intermediateSelected || advancedSelected)
+    showResult();
+    else
+    alert("Please answer all the questions"); 
 }
+
+/**
+ * This function is used to check the radio buttons have been selected before submit.
+ * This check function code below was taken from stackoverflow.
+ * @param {*} question 
+ * @returns 
+ */
+
 function check(question){
     var radios = document.getElementsByName(question);
 
@@ -190,6 +218,9 @@ function check(question){
 
     return false;
 }
+
+// Add event listener to the beginner, intermediate and advanced question button //
+
 let myButton3 = document.getElementById('beginner-question-btn');
 myButton3.addEventListener('click', submitAnswer);
 
@@ -199,6 +230,8 @@ myButton4.addEventListener('click', submitAnswer);
 let myButton5 = document.getElementById('advanced-question-btn');
 myButton5.addEventListener('click', submitAnswer);
 
+// These arrays are used to save the answers of the questions //
+
 const beginnerAnswers = ["Islamabad","Mercury","Krone","Tin","Joe-Biden"];
 const intermediateAnswers = ["Irn-Bru","Four","Once","Olivia","Constantinople"];
 const advancedAnswers = ["4","Knee","Tennessee","China","Call-of-Duty"];
@@ -206,7 +239,9 @@ const advancedAnswers = ["4","Knee","Tennessee","China","Call-of-Duty"];
 /**
  * Checks the answer compare to userAnswer in
  * the returned calculateCorrectAnswer array
-* */
+ * If the answer is incorrect it will give the alert with mistakes and right answers.
+ * It also increment the score and incorrect answers.
+*/
 
 function showResult(){
     let box = document.getElementsByClassName('quiz')[0];
@@ -250,7 +285,6 @@ function showResult(){
             }
         }
     }
-
     let scoreArea = document.getElementsByClassName('score-area');
     for (let index = 0; index < scoreArea.length; index++) 
         scoreArea[index].style.display='block';
@@ -268,6 +302,13 @@ function showResult(){
 
     document.getElementById('back');
     back.style.display='block';
+
+    /**
+     * goBack function is used at the last page
+     * By clicking on the goBack button 
+     * Return the user to the first page
+    */
+
     function goBack(){
         history.go();
      console.log(goBack);
